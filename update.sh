5d7f4b65a17afcab55abd9674ef8454097d2961e
Boris Fomitchev <bfomitchev@nvidia.com>
2016-07-20 17:51:49 -0700

    * extra/cudnn 4667883...7ecc771 (5):   > Updating tests * extra/penlight 16d1493...684c1dd (12):   > all source should now have Unix line endings * pkg/torch 7c740d5...58b9acd (1):   > Merge pull request #720 from apaszke/master

--- update.sh
+++ update.sh
@@ -1,8 +1,38 @@
-# git pull
-# git submodule update
+#!/bin/bash
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+
 git fetch
-git submodule foreach git fetch
-git submodule foreach git checkout master
-git submodule foreach git pull
-# git add extra pkg exe
-# git commit -m "updating packages"
+
+cat .gitmodules |while read i
+do
+if [[ $i == \[submodule* ]]; then
+    echo "Processing $i ..."
+
+    # extract the module's prefix
+    mpath=$(echo $i | cut -d\" -f2)
+
+    # skip two lines
+    read i; read i;
+
+    # extract the url of the submodule
+    murl=$(echo $i|cut -d\  -f3)
+
+    # extract the module name
+    mname=$(basename $mpath)
+
+    read i;
+    mbranch=$(echo $i|cut -d\  -f3)
+
+    # fetch the files
+
+    cd "${THIS_DIR}/${mpath}" && git checkout $mbranch && git pull
+    if [[ $mbranch != "master" ]]; then
+        cd "${THIS_DIR}/${mpath}" && git fetch upstream && git rebase -p upstream/master
+    fi
+fi
+done
+
+cd "${THIS_DIR}"
+pwd
+git status
