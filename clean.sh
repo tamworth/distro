1925fa9e544de2242caf9e624519c9c4ab1541c0
Boris Fomitchev <bfomitchev@nvidia.com>
2016-07-29 00:57:31 -0700

    Preparing for pbuild

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 
 rm -fr install
 
-find "${THIS_DIR}/" -type d -name build  -or -name luarocks.build \
+find "${THIS_DIR}/" -type d -name build  -or -name luarocks.build -or -name build.luarocks \
     | grep -v '/exe/luajit-rocks/' | xargs -I {} rm -rf {}
 
 cd ${THIS_DIR}
